e3ee1429cf1bfc9824020f8aece1345c1e7c3545
Fatihbahce <fathbahce@gmail.com>
2023-04-11 15:50:00 +0300

    database added

--- iletisim.php
+++ iletisim.php
@@ -1,48 +1,39 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $ad = $_POST["ad"];
-    $soyad = $_POST["soyad"];
-    $numara = $_POST["numara"];
-    
-    // Fotoğrafın yüklenip yüklenmediğini kontrol et
-    if(isset($_FILES["foto"]) && $_FILES["foto"]["error"] == 0){
-        $hedef_klasor = "fotograflar/";
-        $hedef_dosya = $hedef_klasor . basename($_FILES["foto"]["name"]);
-        $dosya_tipi = strtolower(pathinfo($hedef_dosya, PATHINFO_EXTENSION));
-        
-        // Fotoğrafı hedef klasöre taşı
-        if(move_uploaded_file($_FILES["foto"]["tmp_name"], $hedef_dosya)){
-            echo "Fotoğraf başarıyla yüklendi.<br>";
-            echo "Yüklenen dosyanın adı: " . $_FILES["foto"]["name"] . "<br>";
-            echo "Dosya tipi: " . $dosya_tipi . "<br>";
-            echo "Dosya boyutu: " . $_FILES["foto"]["size"] . " byte<br>";
-        } else {
-            echo "Fotoğraf yüklenirken hata oluştu.<br>";
+// Veritabanı bağlantı bilgilerini ayarlayın
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "iletisim_formu";
 
+// Veritabanına bağlanın
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        // Dosyayı hedef klasöre kaydet
-        if (move_uploaded_file($_FILES["dosya"]["tmp_name"], $dosyaHedef)) {
-            echo "<script>alert('Dosya başarıyla yüklendi. İletişim formu başarıyla gönderildi.'); window.location.href = 'index.html';</script>";
-        } else {
-            echo "<script>alert('Dosya yüklenirken hata oluştu. Lütfen tekrar deneyin.');</script>";
-        }
-    }
-        }
-    }
-    
-    // E-posta gönderimi
-    $to = "iletisim@orneksite.com"; // E-posta alıcısı
-    $subject = "İletişim Formu"; // E-posta konusu
-    $message = "Ad: " . $ad . "\nSoyad: " . $soyad . "\nNumara: " . $numara; // E-posta içeriği
-    
-    $headers = "From: iletisim@orneksite.com" . "\r\n" .
-        "Reply-To: iletisim@orneksite.com" . "\r\n" .
-        "X-Mailer: PHP/" . phpversion();
-    
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Form başarıyla gönderildi.<br>";
-    } else {
-        echo "İletişim formu gönderilirken hata oluştu.<br>";
-    }
+// Bağlantıyı kontrol edin
+if ($conn->connect_error) {
+    die("Bağlantı hatası: " . $conn->connect_error);
+}
 
+// Form verilerini alın
+$isim = $_POST["isim"];
+$soyisim = $_POST["soyisim"];
+$telefon = $_POST["telefon"];
+$foto = $_FILES["foto"]["name"];
+
+// Fotoğrafı sunucuya yükleyin
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["foto"]["name"]);
+move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file);
+
+// Veri doğrulama işlemlerini yapabilirsiniz
+
+// Veriyi veritabanına kaydedin
+$sql = "INSERT INTO iletisim_verileri (isim, soyisim, telefon, foto) VALUES ('$isim', '$soyisim', '$telefon', '$foto')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Form verileri başarıyla kaydedildi.";
+} else {
+    echo "Hata: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
